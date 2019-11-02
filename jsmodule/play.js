c6747e7e667a6e3b641e2bc3a8c48790f5f5b015
briancollins081 <briancollins081@gmail.com>
2019-11-02 08:40:34 +0300

    JS Refresher: object distructuring.

--- jsmodule/play.js
+++ jsmodule/play.js
@@ -6,30 +6,48 @@ const person = {
     }
 };
 
-copiedPerson = {...person};
-console.log(copiedPerson);
+// copiedPerson = {...person};
+// console.log(copiedPerson);
 const hobbies = ['Sports', 'Cooking'];
-// for (let hobby of hobbies) {
-//     console.log(hobby);
-// }
 
-// console.log(hobbies.map(h =>'Hobby: '+h));
-// console.log(hobbies);
+// // for (let hobby of hobbies) {
+// //     console.log(hobby);
+// // }
 
-// Reference Type can be edited since the const holds the reference to an array or object
-hobbies.push('Programming');
-// console.log(hobbies);
+// // console.log(hobbies.map(h =>'Hobby: '+h));
+// // console.log(hobbies);
 
-//REST and SPREAD Operators
-// copiedArray = hobbies.slice(); //copies an array
+// // Reference Type can be edited since the const holds the reference to an array or object
+// hobbies.push('Programming');
+// // console.log(hobbies);
 
-//spread op
-copiedArray = [...hobbies];
-console.log(copiedArray);
+// //REST and SPREAD Operators
+// // copiedArray = hobbies.slice(); //copies an array
 
-//rest op
-const toArray = (...args) => {
-    return args;
-};
+// //spread op
+// copiedArray = [...hobbies];
+// console.log(copiedArray);
 
-console.log(toArray(1,2,3,4));
+// //rest op
+// const toArray = (...args) => {
+//     return args;
+// };
+
+// console.log(toArray(1,2,3,4));
+
+const printName = (personD) => {
+    console.log(personD.name);
+}
+printName(person);
+
+const printName2 = ({name}) => {
+    console.log(name);
+}
+printName2(person);
+
+const {name, age} = person;
+
+console.log(name, age);
+
+const [hobby1, hobby2] = hobbies; //uses any names you want in relation to position
+console.log(hobby1, hobby2);
