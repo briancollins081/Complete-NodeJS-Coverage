db7b14ec4415a641b42c39ba611657600562f307
briancollins081 <briancollins081@gmail.com>
2019-11-02 09:04:27 +0300

    JS Refresher: Using Asynchronous code

--- jsmodule/play.js
+++ jsmodule/play.js
@@ -1,53 +1,24 @@
-const person = {
-    name: "Andere",
-    age: 29,
-    greet() {
-        console.log("Hi, I am "+this.name);
-    }
+// Asynchronous
+const fetchData = () => {
+    const promise = new Promise((resolve, reject)=>{
+         setTimeout(() => {
+             resolve('Done Waiting, Executed!')
+         }, 1500);
+    });
+    return promise;
 };
+setTimeout(() => {
+    console.log('Timer is done');
+    fetchData().then(text => {
+        console.log(text);
+        return fetchData();
+    })
+    .then(text2=>{
+        console.log(text2);
+    });
+    
+}, 2000);
 
-// copiedPerson = {...person};
-// console.log(copiedPerson);
-const hobbies = ['Sports', 'Cooking'];
-
-// // for (let hobby of hobbies) {
-// //     console.log(hobby);
-// // }
-
-// // console.log(hobbies.map(h =>'Hobby: '+h));
-// // console.log(hobbies);
-
-// // Reference Type can be edited since the const holds the reference to an array or object
-// hobbies.push('Programming');
-// // console.log(hobbies);
-
-// //REST and SPREAD Operators
-// // copiedArray = hobbies.slice(); //copies an array
-
-// //spread op
-// copiedArray = [...hobbies];
-// console.log(copiedArray);
-
-// //rest op
-// const toArray = (...args) => {
-//     return args;
-// };
-
-// console.log(toArray(1,2,3,4));
-
-const printName = (personD) => {
-    console.log(personD.name);
-}
-printName(person);
-
-const printName2 = ({name}) => {
-    console.log(name);
-}
-printName2(person);
-
-const {name, age} = person;
-
-console.log(name, age);
-
-const [hobby1, hobby2] = hobbies; //uses any names you want in relation to position
-console.log(hobby1, hobby2);
+//Synchronous executed while waiting for async code
+console.log('Hello');
+console.log('Hi');
