1646f0c7d2b601573677c9ec0b8094e73c4df543
briancollins081 <briancollins081@gmail.com>
2019-11-02 08:05:44 +0300

    JS Refresher: Working with objects

--- jsmodule/play.js
+++ jsmodule/play.js
@@ -1,18 +1,9 @@
-const name = "Max";
-let age = 29;
-const hasHobbies = true;
+const person = {
+    name: "Andere",
+    age: 29,
+    greet() {
+        console.log("Hi, I am "+this.name);
+    }
+};
 
-age = 30;
-
-const summarizeUser = (userName, userAge, userHasHobby) => {
-    return ('Name is '+userName+' age is '+userAge+' and user has hobbies: '+userHasHobby);
-}
-
-
-// const add = (a, b) => a+b;
-// const addOne = a => a+1;
-const addRandom = () => 1+2;
-
-console.log(summarizeUser(name, age, hasHobbies));
-
-console.log(addRandom());
+person.greet();
