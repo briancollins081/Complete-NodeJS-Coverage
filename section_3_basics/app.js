01ef674dba00eade176b89e1b89ca53f04cf61e8
briancollins081 <briancollins081@gmail.com>
2019-11-06 08:25:12 +0300

    Listen to requsts and return response

--- section_3_basics/app.js
+++ section_3_basics/app.js
@@ -2,12 +2,19 @@ const http = require('http');
 
 
 const server = http.createServer((req,res)=>{
-    console.log(req.url, req.method, req.headers);
-    // process.exit();
+    const url = req.url;
+    if(url==='/'){
+        res.write('<html>');
+        res.write('<head><title>Enter Message</title></head>');
+        res.write('<body><form action="/message" method="POST"><input type="text" name="message"/> <button type="submit">Send</button></form></body>');
+        res.write('</html>');
+        return res.end(); //end at this point due to end()
+    }
+
     res.setHeader('Content-Type','text/html');
     res.write('<html>');
     res.write('<head><title>First Page: Node</title></head>');
-    res.write('<body><h1>Hello there this is </h1></body>');
+    res.write('<body><h1>Hello there this is ABC </h1></body>');
     res.write('</html>');
     res.end();
 }); 
