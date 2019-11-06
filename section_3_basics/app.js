bd701b7e9b53c52c5f74241090bbe86b8c82c305
briancollins081 <briancollins081@gmail.com>
2019-11-06 09:44:41 +0300

    Managing files and exports with Node Module System

--- section_3_basics/app.js
+++ section_3_basics/app.js
@@ -1,46 +1,6 @@
 const http = require('http');
-const fs = require('fs');
+const routes = require('./routes');
 
-
-const server = http.createServer((req,res)=>{
-    const url = req.url;
-    const method = req.method;
-    if(url==='/'){
-        res.write('<html>');
-        res.write('<head><title>Enter Message</title></head>');
-        res.write('<body><form action="/message" method="POST"><input type="text" name="message"/> <button type="submit">Send</button></form></body>');
-        res.write('</html>');
-        return res.end(); //end at this point due to end()
-    }
-    if(url === '/message' && method === 'POST'){
-        //get request data before writing to the file
-        const body = [];
-        //fetch data chunks
-        req.on('data', (chunk)=>{
-            console.log(chunk);
-            body.push(chunk);
-        });
-        //work on the data fetched from the post method
-        return req.on('end', ()=>{
-            const parseBody = Buffer.concat(body).toString();
-            const message = parseBody.split('=')[1];
-            console.log(parseBody);
-            fs.writeFile('message.txt', message, (err)=>{
-                res.statusCode = 302;
-                res.setHeader('Location', '/');
-                return res.end();
-            }); // blocks until write is done
-            
-        });
-        
-    }
-    if(url==='message')
-    res.setHeader('Content-Type','text/html');
-    res.write('<html>');
-    res.write('<head><title>First Page: Node</title></head>');
-    res.write('<body><h1>Hello there this is ABC </h1></body>');
-    res.write('</html>');
-    res.end();
-}); 
-
+const server = http.createServer(routes.handler);
+console.log(routes.someText);
 server.listen(3000);
