e47999efec178a0da18820461cf1c5d8898eb6e8
briancollins081 <briancollins081@gmail.com>
2019-11-06 08:57:37 +0300

    Parsing response from node

--- section_3_basics/app.js
+++ section_3_basics/app.js
@@ -1,8 +1,10 @@
 const http = require('http');
+const fs = require('fs');
 
 
 const server = http.createServer((req,res)=>{
     const url = req.url;
+    const method = req.method;
     if(url==='/'){
         res.write('<html>');
         res.write('<head><title>Enter Message</title></head>');
@@ -10,7 +12,27 @@ const server = http.createServer((req,res)=>{
         res.write('</html>');
         return res.end(); //end at this point due to end()
     }
+    if(url === '/message' && method === 'POST'){
+        //get request data before writing to the file
+        const body = [];
+        //fetch data chunks
+        req.on('data', (chunk)=>{
+            console.log(chunk);
+            body.push(chunk);
+        });
+        //work on the data fetched from the post method
+        req.on('end', ()=>{
+            const parseBody = Buffer.concat(body).toString();
+            const message = parseBody.split('=')[1];
+            console.log(parseBody);
+            fs.writeFileSync('message.txt', message);
 
+        });
+        res.statusCode = 302;
+        res.setHeader('Location', '/');
+        return res.end();
+    }
+    if(url==='message')
     res.setHeader('Content-Type','text/html');
     res.write('<html>');
     res.write('<head><title>First Page: Node</title></head>');
