04e95f5361b8724f7d3894ceecd5b5fb4240493c
briancollins081 <briancollins081@gmail.com>
2019-11-06 08:09:24 +0300

    Handling response data

--- section_3_basics/app.js
+++ section_3_basics/app.js
@@ -4,6 +4,12 @@ const http = require('http');
 const server = http.createServer((req,res)=>{
     console.log(req.url, req.method, req.headers);
     // process.exit();
-});
+    res.setHeader('Content-Type','text/html');
+    res.write('<html>');
+    res.write('<head><title>First Page: Node</title></head>');
+    res.write('<body><h1>Hello there this is </h1></body>');
+    res.write('</html>');
+    res.end();
+}); 
 
 server.listen(3000);
