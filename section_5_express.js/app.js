1003914070ef9f62c106f9e15541bfc72b6c23f4
briancollins081 <briancollins081@gmail.com>
2019-11-07 20:14:47 +0300

    Handling different routes: The always runs route | default for all other routes&& The specific ones + their order code arrangement.

--- section_5_express.js/app.js
+++ section_5_express.js/app.js
@@ -1,19 +1,20 @@
-// const http = require('http');
-
 const express = require('express');
 
 const app = express();
-
-app.use((req, res, next)=>{
-    console.log("In the middleware!");
-    next(); // allows execution to move to the next middleware following this
+app.use('/', (req, res, next)=>{
+    console.log('This always runs!!!');
+    next();
 });
 
-app.use((req, res, next) => {
+app.use('/add-product', (req, res, next) => {
+    console.log("In another middleware!");
+    res.send('<h1>The "Add Product" Page</h1>');
+});
+
+app.use('/', (req, res, next) => {
     console.log("In another middleware!");
     res.send('<h1>Hello from express</h1>');
 });
 
-// const server = http.createServer(app);
-// server.listen(3000);
+
 app.listen(3000);
