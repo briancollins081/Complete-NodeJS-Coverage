b27e3b2e1959d5fb418026fac441234d7d2483d1
briancollins081 <briancollins081@gmail.com>
2019-11-08 07:15:19 +0300

    Adding 404 Page not found

--- section_5_express.js/app.js
+++ section_5_express.js/app.js
@@ -11,4 +11,8 @@ app.use(bodyParser.urlencoded({extended: false}));// add a parser for the body i
 app.use(adminRoutes);
 app.use(shopeRoutes);
 
+app.use('/', (req, res, next)=>{
+    res.status(404).send('<h1>Page not found</h1>');
+});
+
 app.listen(3000);
