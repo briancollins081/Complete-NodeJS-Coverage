f4c86a4c893cae46618af4893f7fab3986234467
briancollins081 <briancollins081@gmail.com>
2019-11-08 07:25:10 +0300

    Filtering routes - using an additional path

--- section_5_express.js/app.js
+++ section_5_express.js/app.js
@@ -8,7 +8,7 @@ const shopeRoutes = require('./routes/shop');
 
 app.use(bodyParser.urlencoded({extended: false}));// add a parser for the body incomming request
 
-app.use(adminRoutes);
+app.use('/admin', adminRoutes);
 app.use(shopeRoutes);
 
 app.use('/', (req, res, next)=>{
--- section_5_express.js/routes/admin.js
+++ section_5_express.js/routes/admin.js
@@ -2,10 +2,12 @@ const express = require('express');
 
 const router = express.Router();
 
+// /admin/add-product => GET -reaches this route
 router.get('/add-product', (req, res, next) => {
-    res.send("<form name='addproduct' action='/product' method='post'><input type='text' name='title'><button type='submit'>Add a Product</button></form>");
+    res.send("<form name='addproduct' action='/admin/add-product' method='post'><input type='text' name='title'><button type='submit'>Add a Product</button></form>");
 });
-router.post('/product', (req, res, next) => {
+// /admin/add-product => POST reaches this one
+router.post('/add-product', (req, res, next) => {
     console.log(req.body);
     res.redirect('/');
 });
