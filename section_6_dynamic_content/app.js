2b86ea9a381f2df4053d50c9fa199117817a05f5
briancollins081 <briancollins081@gmail.com>
2019-11-09 23:00:34 +0300

    EJS - Working with EJS Used for the rest of the course

--- section_6_dynamic_content/app.js
+++ section_6_dynamic_content/app.js
@@ -2,12 +2,10 @@ const path = require('path');
 
 const express = require('express');
 const bodyParser = require('body-parser');
-const expressHbs = require('express-handlebars');
 
 const app = express();
 
-app.engine('hbs', expressHbs({layoutsDir: 'views/layouts', defaultLayout: 'main-layout', extname:'hbs'})); // engine not defined within the node library
-app.set('view engine', 'hbs');
+app.set('view engine', 'ejs');
 app.set('views','views');
 
 const adminData = require('./routes/admin');
@@ -21,7 +19,9 @@ app.use(shopRoutes);
 
 app.use('/', (req, res, next)=>{
     // res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
-    res.status(404).render('404',{docTitle: 'Resource not found: 404'});
+    res.status(404).render('404',{
+        docTitle: 'Resource not found: 404'
+    });
 });
 
 app.listen(3000);
