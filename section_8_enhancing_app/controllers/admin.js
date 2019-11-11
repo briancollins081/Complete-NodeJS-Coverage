4f11b3d25480f2b44a1a895253af60e2ee418e74
briancollins081 <briancollins081@gmail.com>
2019-11-11 15:56:17 +0300

    Registering the application routes that were added

--- section_8_enhancing_app/controllers/admin.js
+++ section_8_enhancing_app/controllers/admin.js
@@ -18,13 +18,10 @@ exports.postAddProduct = (req, res, next) => {
 
 exports.getProducts = (req, res, next) => {
     Product.fetchAll(products => {
-        res.render('shop/product-list', {
+        res.render('admin/products', {
             prods: products,
-            docTitle: 'Shop',
-            path: '/',
-            hasProducts: products.length > 0,
-            activeShop: true,
-            productCSS: true
+            docTitle: 'Admin Products',
+            path: '/admin/products'
         });
     });
 }
--- section_8_enhancing_app/routes/admin.js
+++ section_8_enhancing_app/routes/admin.js
@@ -2,15 +2,19 @@ const path = require('path');
 
 const express = require('express');
 
-const productsController = require('../controllers/products');
+const adminController = require('../controllers/admin');
 
 const router = express.Router();
 
 
 
 // /admin/add-product => GET -reaches this route
-router.get('/add-product', productsController.getAddProduct);
+router.get('/add-product', adminController.getAddProduct);
+
+// admin /products => GET - reaches this route
+router.get('/products', adminController.getProducts);
+
 // /admin/add-product => POST reaches this one
-router.post('/add-product', productsController.postAddProduct);
+router.post('/add-product', adminController.postAddProduct);
 
 module.exports = router;
