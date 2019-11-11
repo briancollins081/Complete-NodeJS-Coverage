0ba29f4265b2d4fc30870116be24eb50cbabb1ea
briancollins081 <briancollins081@gmail.com>
2019-11-11 08:52:43 +0300

    Using models to save data to files Fetching and storing files to json file

--- section_7_mvc/models/product.js
+++ section_7_mvc/models/product.js
@@ -1,4 +1,7 @@
-const products = [];
+const fs = require('fs');
+const path = require('path');
+
+const rootDirectory = require('../util/path');
 
 module.exports = class Product {
     constructor(t){
@@ -6,10 +9,35 @@ module.exports = class Product {
     }
 
     save(){
-        products.push(this);
+        const p=path.join(
+            rootDirectory, 
+            'data', 
+            'products.json'
+        );
+        fs.readFile(p, (err, fileContent)=>{
+            let products = [];
+            if(!err){
+                products = JSON.parse(fileContent);
+            }
+
+            products.push(this);
+            fs.writeFile(p, JSON.stringify(products),(err)=>{
+                console.log(err);
+            });
+        });
     }
 
-    static fetchAll(){
-        return products;
+    static fetchAll(cb){
+        const p = path.join(
+            rootDirectory,
+            'data',
+            'products.json'
+        );
+        fs.readFile(p,(err, fileContent)=>{
+            if(err){
+                cb([]);
+            }
+            cb(JSON.parse(fileContent));
+        });
     }
 }
