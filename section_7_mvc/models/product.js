f6468071b78f1037f634964ca73fb82aea0ffaa0
briancollins081 <briancollins081@gmail.com>
2019-11-11 09:17:53 +0300

    File storage code: refractoring.

--- section_7_mvc/models/product.js
+++ section_7_mvc/models/product.js
@@ -3,41 +3,37 @@ const path = require('path');
 
 const rootDirectory = require('../util/path');
 
+const p = path.join(
+    rootDirectory,
+    'data',
+    'products.json'
+);
+
+const getProductsFromFile = (cb) =>{
+    fs.readFile(p, (err, fileContent) => {
+        if (err) {
+            cb([]);
+        }else{
+            cb(JSON.parse(fileContent));
+        }  
+    });
+}
+
 module.exports = class Product {
     constructor(t){
         this.title = t;
     }
 
     save(){
-        const p=path.join(
-            rootDirectory, 
-            'data', 
-            'products.json'
-        );
-        fs.readFile(p, (err, fileContent)=>{
-            let products = [];
-            if(!err){
-                products = JSON.parse(fileContent);
-            }
-
+        getProductsFromFile(products =>{
             products.push(this);
-            fs.writeFile(p, JSON.stringify(products),(err)=>{
+            fs.writeFile(p, JSON.stringify(products), (err) => {
                 console.log(err);
             });
         });
     }
 
     static fetchAll(cb){
-        const p = path.join(
-            rootDirectory,
-            'data',
-            'products.json'
-        );
-        fs.readFile(p,(err, fileContent)=>{
-            if(err){
-                cb([]);
-            }
-            cb(JSON.parse(fileContent));
-        });
+        getProductsFromFile(cb);
     }
 }
