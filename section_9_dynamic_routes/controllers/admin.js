6d84374a22f0421bbf53873d421bf7b3607d9441
briancollins081 <briancollins081@gmail.com>
2019-11-13 13:03:17 +0300

    Using query params & pre populating edit product page with data

--- section_9_dynamic_routes/controllers/admin.js
+++ section_9_dynamic_routes/controllers/admin.js
@@ -1,12 +1,11 @@
 const Product = require('../models/product');
 
 exports.getAddProduct = (req, res, next) => {
-  res.render('admin/add-product', {
+  const editMode = false;
+  res.render('admin/edit-product', {
     pageTitle: 'Add Product',
-    path: '/admin/add-product',
-    formsCSS: true,
-    productCSS: true,
-    activeAddProduct: true
+    path: '/admin/edit-product',
+    editing: editMode
   });
 };
 
@@ -18,6 +17,26 @@ exports.postAddProduct = (req, res, next) => {
   const product = new Product(title, imageUrl, description, price);
   product.save();
   res.redirect('/');
+};
+
+exports.getEditProduct = (req, res, next) => {
+  const editMode = req.query.edit;
+  if(!editMode){
+    return res.redirect('/');
+  }
+  const prodId = req.params.productId;
+  Product.findById(prodId, product => {
+    if(!product){
+      return res.redirect('/');
+    }
+    res.render('admin/edit-product', {
+      pageTitle: 'Edit Product',
+      path: '/admin/edit-product',
+      editing: editMode,
+      product: product
+    });
+  });
+  
 };
 
 exports.getProducts = (req, res, next) => {
