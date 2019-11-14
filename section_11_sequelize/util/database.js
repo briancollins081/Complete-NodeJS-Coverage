39a9f180626528f846043428dedf74ed06108ece
briancollins081 <briancollins081@gmail.com>
2019-11-14 10:30:56 +0300

    Sequelize - Insert data & create product

--- section_11_sequelize/util/database.js
+++ section_11_sequelize/util/database.js
@@ -1,8 +1,7 @@
-const Sequelize = require('sequelize'); //constructor function
+const Sequelize = require('sequelize').Sequelize; //constructor function
 
 const sequelize = new Sequelize('node-complete', 'nodejs', 'Nodejs-complete', {
     dialect: 'mysql',
     host: 'localhost'
 });
-
 module.exports = sequelize;
