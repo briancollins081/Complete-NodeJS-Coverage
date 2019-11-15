ad2729c3a9f6eb976d36829769d01a5316d9f362
briancollins081 <briancollins081@gmail.com>
2019-11-15 18:28:16 +0300

    Fetching related products This is based on the user we created.

--- section_11_sequelize/controllers/admin.js
+++ section_11_sequelize/controllers/admin.js
@@ -36,7 +36,10 @@ exports.getEditProduct = (req, res, next) => {
     return res.redirect('/');
   }
   const prodId = req.params.productId;
-  Product.findByPk(prodId).then(product => {
+  req.user.getProducts({where: { id: prodId } })
+  // Product.findByPk(prodId)
+  .then(products => {
+    const product=products[0];
     if(!product){
       return res.redirect('/');
     }
@@ -85,7 +88,8 @@ exports.postDeleteProduct = (req, res, next) => {
   .catch(err=>console.log(err));
 };
 exports.getProducts = (req, res, next) => {
-  Product.findAll()
+  req.user.getProducts()
+  // Product.findAll()
     .then(products=>{
        res.render('admin/products', {
          prods: products,
