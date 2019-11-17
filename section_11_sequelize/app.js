d8eb80f416b013f10bb64363b08416005f1059af
briancollins081 <briancollins081@gmail.com>
2019-11-17 09:42:48 +0300

    Adding the order model  - Order model that will manage the orders table - OrderItem model that will manage the many to many relationship between a product and an order

--- section_11_sequelize/app.js
+++ section_11_sequelize/app.js
@@ -9,6 +9,8 @@ const Product = require('./models/product');
 const User = require('./models/user');
 const Cart = require('./models/cart');
 const CartItem = require('./models/cart-item');
+const Order = require('./models/order');
+const OrderItem = require('./models/order-item');
 
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
@@ -41,6 +43,12 @@ Cart.belongsTo(User); // adds userID to the cart
 
 Cart.belongsToMany(Product, { through: CartItem });
 Product.belongsToMany(Cart, { through: CartItem });
+
+Order.belongsTo(User);
+User.hasMany(Order);
+
+Order.belongsToMany(Product, {through: OrderItem});
+
 
 // sequelize.sync({ force: true }) //recreates a the database enforcing all the new associations
 sequelize.sync()
