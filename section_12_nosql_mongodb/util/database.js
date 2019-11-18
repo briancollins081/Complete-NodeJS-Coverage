d63f9b67c91689c551e12b6085e78d3ca5943fac
briancollins081 <briancollins081@gmail.com>
2019-11-18 06:43:22 +0300

    Installing Mongo DB Driver  - connecting to the mongodb cloud server using connection url

--- section_12_nosql_mongodb/util/database.js
+++ section_12_nosql_mongodb/util/database.js
@@ -1,7 +1,23 @@
-const Sequelize = require('sequelize').Sequelize; //constructor function
+// SEquelize
+// const Sequelize = require('sequelize').Sequelize; //constructor function
 
-const sequelize = new Sequelize('node-complete', 'nodejs', 'Nodejs-complete', {
-    dialect: 'mysql',
-    host: 'localhost'
-});
-module.exports = sequelize;
+// const sequelize = new Sequelize('node-complete', 'nodejs', 'Nodejs-complete', {
+//     dialect: 'mysql',
+//     host: 'localhost'
+// });
+// module.exports = sequelize;
+
+//Mongo Db
+const mongodb = require('mongodb');
+const MongoClient = mongodb.MongoClient;
+const mongoConnect = (callback) => {
+    MongoClient.connect('mongodb+srv://abcnodejs:nodejs-complete@cluster0-h0swz.mongodb.net/test?retryWrites=true&w=majority')
+        .then(client => {
+            console.log("connected!");
+            callback(client);
+        })
+        .catch(err => console.log(err));
+};
+
+module.exports = mongoConnect;
+
