88b972932a2d595fbaa459882c9c4f4de7b9c9bf
Brian Andere <briancollins081@gmail.com>
2019-11-27 13:39:23 +0300

    Working on the cart and the cart items Added a method that can add a product to the user bound cart object The added product overrides the existing products if any.

--- section_12_nosql_mongodb/models/user.js
+++ section_12_nosql_mongodb/models/user.js
@@ -2,9 +2,10 @@ const getDb = require("../util/database").getDb;
 const mongodb = require("mongodb");
 
 class User {
-    constructor(username, email) {
+    constructor(username, email, cart) {
         this.username = username;
         this.email = email;
+        this.cart = cart; //An object with items: cart = {items:[]}
     }
 
     static findById(userId) {
@@ -12,7 +13,15 @@ class User {
         return db.collection("users")
             .findOne({ _id: new mongodb.ObjectId(userId) });
     }
-
+    addToCart(product) {
+        // //check the cart object to see if the product exists in it
+        // const cartProduct = this.cart.findIndex(cp => {
+        //     cp._id === product._id;
+        // });
+        const updatedCart = { items: [{...product, quantity: 1 }] };
+        const db = getDb();
+        return db.collection('users').updateOne({ _id: new mongodb.ObjectId(this._id) }, { $set: { cart: updatedCart } });
+    }
     save() {
         const db = getDb();
         return db.collection("users").insertOne(this);
