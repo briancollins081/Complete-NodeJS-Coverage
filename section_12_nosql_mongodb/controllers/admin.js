7999787b9e3464b501eeb4854367101aaeff2008
Brian Andere <briancollins081@gmail.com>
2019-11-27 05:53:39 +0300

    Making edit and delete product buttons work again Workinh with the Product model to edit  our product Changed the id to mongo db format in the views

--- section_12_nosql_mongodb/controllers/admin.js
+++ section_12_nosql_mongodb/controllers/admin.js
@@ -27,72 +27,70 @@ exports.postAddProduct = (req, res, next) => {
         });
 };
 
-// exports.getEditProduct = (req, res, next) => {
-//   const editMode = req.query.edit;
-//   if(!editMode){
-//     return res.redirect('/');
-//   }
-//   const prodId = req.params.productId;
-//   req.user.getProducts({where: { id: prodId } })
-//   // Product.findByPk(prodId)
-//   .then(products => {
-//     const product=products[0];
-//     if(!product){
-//       return res.redirect('/');
-//     }
-//     res.render('admin/edit-product', {
-//       pageTitle: 'Edit Product',
-//       path: '/admin/edit-product',
-//       editing: editMode,
-//       product: product
-//     });
-//   })
-//   .catch(err=>console.log(err));
-// };
+exports.getEditProduct = (req, res, next) => {
+    const editMode = req.query.edit;
+    if (!editMode) {
+        return res.redirect('/');
+    }
+    const prodId = req.params.productId;
+    Product.findById(prodId)
+        .then(product => {
+            if (!product) {
+                return res.redirect('/');
+            }
+            res.render('admin/edit-product', {
+                pageTitle: 'Edit Product',
+                path: '/admin/edit-product',
+                editing: editMode,
+                product: product
+            });
+        })
+        .catch(err => console.log(err));
+};
 
-// exports.postEditProduct = (req, res, next) =>{
-//   //fetch info for the product
-//   const prodId = req.body.productId;
-//   const utitle = req.body.title;
-//   const uprice = req.body.price;
-//   const uimageurl = req.body.imageUrl;
-//   const udescritpion = req.body.description;
-//   Product.findByPk(prodId)
-//     .then(product => {
-//       product.title = utitle;
-//       product.price = uprice;
-//       product.imageUrl = uimageurl;
-//       product.description = udescritpion;
-//       return product.save(); //updates or create the object
-//     })
-//     .then(product=>{
-//       console.log("Product UPDATED!!");
-//       res.redirect('/admin/products');
-//     })
-//     .catch(err=>console.log(err));
+exports.postEditProduct = (req, res, next) => {
+    //fetch info for the product
+    const prodId = req.body.productId;
+    const utitle = req.body.title;
+    const uprice = req.body.price;
+    const uimageurl = req.body.imageUrl;
+    const udescritpion = req.body.description;
+    Product.findById(prodId)
+        .then(product => {
+            product.title = utitle;
+            product.price = uprice;
+            product.imageUrl = uimageurl;
+            product.description = udescritpion;
+            return product.save(); //updates or create the object
+        })
+        .then(product => {
+            console.log("Product UPDATED!!");
+            res.redirect('/admin/products');
+        })
+        .catch(err => console.log(err));
+};
 
-// };
-// exports.postDeleteProduct = (req, res, next) => {
-//   const productId=req.body.productId;
-//   Product.findByPk(productId)
-//   .then(product => {
-//     return product.destroy();
-//   })
-//   .then(result => {
-//     console.log('Destroyed Product!!');
-//     res.redirect('/admin/products');
-//   })
-//   .catch(err=>console.log(err));
-// };
-// exports.getProducts = (req, res, next) => {
-//   req.user.getProducts()
-//   // Product.findAll()
-//     .then(products=>{
-//        res.render('admin/products', {
-//          prods: products,
-//          pageTitle: 'Admin Products',
-//          path: '/admin/products'
-//        });
-//     })
-//     .catch(err=>console.log(err));
-// };
+exports.postDeleteProduct = (req, res, next) => {
+    const productId = req.body.productId;
+    Product.findByPk(productId)
+        .then(product => {
+            return product.destroy();
+        })
+        .then(result => {
+            console.log('Destroyed Product!!');
+            res.redirect('/admin/products');
+        })
+        .catch(err => console.log(err));
+};
+exports.getProducts = (req, res, next) => {
+    //req.user.getProducts()
+    Product.fetchAll()
+        .then(products => {
+            res.render('admin/products', {
+                prods: products,
+                pageTitle: 'Admin Products',
+                path: '/admin/products'
+            });
+        })
+        .catch(err => console.log(err));
+};
