06f9967e1e1cb6f81e730a161f6c65c7f03673f4
Brian Andere <briancollins081@gmail.com>
2019-12-01 13:10:46 +0300

    Added a user model ad configured it in the app.js

--- section_13_working_with_mongoose/app.js
+++ section_13_working_with_mongoose/app.js
@@ -7,7 +7,7 @@ const mongoose = require("mongoose");
 
 const errorController = require("./controllers/error");
 
-// const User = require("./models/user.js");
+const User = require("./models/user.js");
 
 const adminRoutes = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
@@ -18,15 +18,15 @@ app.set("views", "views");
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
-// app.use((req, res, next) => {
-//     User.findById("5ddf58153efdfb4712ef78b2")
-//         .then((user) => {
-//             const cart = user.cart ? user.cart : { items: [] };
-//             req.user = new User(user.name, user.email, cart, user._id);
-//             next();
-//         })
-//         .catch((err) => console.log(err));
-// });
+app.use((req, res, next) => {
+    User.findById("5de38ed0a6675b39a17d0056")
+        .then((user) => {
+            const cart = user.cart ? user.cart : { items: [] };
+            req.user = new User(user.name, user.email, cart, user._id);
+            next();
+        })
+        .catch((err) => console.log(err));
+});
 app.use("/admin", adminRoutes);
 app.use(shopRoutes);
 
@@ -34,6 +34,17 @@ app.use(errorController.get404);
 
 mongoose.connect('mongodb+srv://abcnodejs:nodejs-complete@cluster0-h0swz.mongodb.net/shop?retryWrites=true&w=majority')
     .then(result => {
+        User.findOne().then(user => {
+            if (!user) {
+                const user = new User({
+                    name: "Andere Brian",
+                    email: "briancollins@ac.ke",
+                    cart: []
+                });
+                user.save();
+            }
+        });
+
         app.listen(3000);
     })
     .catch(err => {
