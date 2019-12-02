5c29d3a65182b4a7bc91088703414d44f503cecd
Brian Andere <briancollins081@gmail.com>
2019-12-02 07:21:51 +0300

    Deleting Cart Items

--- section_13_working_with_mongoose/models/user.js
+++ section_13_working_with_mongoose/models/user.js
@@ -50,6 +50,13 @@ userSchema.methods.addToCart = function(product) {
     return this.save();
 }
 
+userSchema.methods.removeFromCart = function(productId) {
+    const updatedCartItems = this.cart.items.filter(i => {
+        return i.productId.toString() !== productId.toString();
+    });
+    this.cart.items = updatedCartItems;
+    return this.save();
+}
 module.exports = mongoose.model('User', userSchema);
 
 // const getDb = require("../util/database").getDb;
