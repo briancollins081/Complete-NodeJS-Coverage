d478ccef18e305227055e23718607191b9dd76f6
Brian Andere <briancollins081@gmail.com>
2019-12-04 06:20:51 +0300

    Request driven login

--- section_14_sessions_cookies/controllers/auth.js
+++ section_14_sessions_cookies/controllers/auth.js
@@ -1,11 +1,12 @@
 exports.getLogin = (req, res, next) => {
     res.render('auth/login', {
         path: '/login',
-        pageTitle: 'Login'
+        pageTitle: 'Login',
+        isAuthenticated: req.loggedIn
     });
 }
 
 exports.postLogin = (req, res, next) => {
-
+    req.loggedIn = true;
     res.redirect('/');
 }
