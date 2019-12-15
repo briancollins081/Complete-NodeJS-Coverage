01638d6ff969dd47dc4fed0b9c4a04a8436dfd0f
Brian Andere <briancollins081@gmail.com>
2019-12-15 06:00:29 +0300

    Accessing error messages from the 'express-validator' using one of our fields - signup email

--- section_18_validation/controllers/auth.js
+++ section_18_validation/controllers/auth.js
@@ -3,6 +3,7 @@ const crypto = require('crypto');
 const bcrypt = require('bcryptjs');
 const nodemailer = require('nodemailer');
 const sendgridTransport = require('nodemailer-sendgrid-transport');
+const { validationResult } = require('express-validator');
 
 const User = require('../models/user');
 const transporter = nodemailer.createTransport(sendgridTransport({
@@ -75,6 +76,17 @@ exports.postSignup = (req, res, next) => {
     const email = req.body.email;
     const password = req.body.password;
     const confirmPassword = req.body.confirmPassword;
+    const errors = validationResult(req);
+
+    if(!errors.isEmpty()){
+        console.log(errors.array());
+        return res.status(422)
+            .render('auth/signup', {
+                path: '/signup',
+                pageTitle: 'Signup',
+                errorMessage: errors.array()
+            });
+    }
 
     User.findOne({ email: email })
         .then(userDocument => {
