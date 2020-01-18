e762eba106f0e8c64ed27c785711ef1526fe3f34
Brian Andere <briancollins081@gmail.com>
2020-01-18 13:48:11 +0300

    Added Headers to server response to allow cross origin responses from the clients by setting a general middleware in app.js

--- section_24_REST_APIs_Basics/app.js
+++ section_24_REST_APIs_Basics/app.js
@@ -8,6 +8,14 @@ const feedRoutes = require('./router/feed');
 // app.use(bodyParser.urlencoded()); // x-www-form-urlencoded <<form post req>>
 app.use(bodyParser.json()); //for json format data
 
+//set headers for all our server requests
+app.use((req, res, next)=>{
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    next();
+})
+
 app.use('/feed', feedRoutes);
 
 app.listen(8080);
