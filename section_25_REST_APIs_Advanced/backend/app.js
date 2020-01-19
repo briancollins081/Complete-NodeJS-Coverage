2497bb4d67b5a5ea77ec44d93c03328c6c192505
Brian Andere <briancollins081@gmail.com>
2020-01-19 20:36:05 +0300

    Creating a mongoose post model for the database postmessages

--- section_25_REST_APIs_Advanced/backend/app.js
+++ section_25_REST_APIs_Advanced/backend/app.js
@@ -1,15 +1,17 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
 const app = express();
 
 const feedRoutes = require('./router/feed');
+const ENV_KEYS = require('./keys/keys');
 
 // app.use(bodyParser.urlencoded()); // x-www-form-urlencoded <<form post req>>
 app.use(bodyParser.json()); //for json format data
 
 //set headers for all our server requests
-app.use((req, res, next)=>{
+app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
@@ -18,4 +20,10 @@ app.use((req, res, next)=>{
 
 app.use('/feed', feedRoutes);
 
-app.listen(8080);
+mongoose.connect(ENV_KEYS.MONGO_DB_URL, { useUnifiedTopology: true, useNewUrlParser: true })
+    .then(result => {
+        app.listen(8080);
+    })
+    .catch(err => {
+        console.log(err);
+    });
