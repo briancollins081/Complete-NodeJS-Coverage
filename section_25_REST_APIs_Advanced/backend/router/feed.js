2b6b3bd95d9eff9d9c932144d8d5787440606bd0
Brian Andere <briancollins081@gmail.com>
2020-01-30 07:07:44 +0300

    Adding auth guard to all routes - a middleware that confirms authentication of the user

--- section_25_REST_APIs_Advanced/backend/router/feed.js
+++ section_25_REST_APIs_Advanced/backend/router/feed.js
@@ -8,18 +8,18 @@ const feedsController = require('../controllers/feed');
 
 router.get('/posts', isAuth, feedsController.getPosts);
 
-router.post('/post', [
+router.post('/post', isAuth, [
     body('title').trim().isLength({ min: 5 }),
     body('content').trim().isLength({ min: 5 }),
 ], feedsController.createPost);
 
-router.get('/post/:postId', feedsController.getPost);
+router.get('/post/:postId', isAuth, feedsController.getPost);
 
-router.put('/post/:postId', [
+router.put('/post/:postId', isAuth, [
     body('title').trim().isLength({ min: 5 }),
     body('content').trim().isLength({ min: 5 }),
 ], feedsController.updatePost);
 
-router.delete('/post/:postId', feedsController.deletePost);
+router.delete('/post/:postId', isAuth, feedsController.deletePost);
 
 module.exports = router;
