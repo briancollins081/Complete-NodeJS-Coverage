61ed6cd50b302f9502de90c396bd46471c478c14
Brian Andere <briancollins081@gmail.com>
2020-02-13 07:30:42 +0300

    Adding input validation for the resolvers using the validator package

--- section_28_GraphQl/backend/graphql/resolvers.js
+++ section_28_GraphQl/backend/graphql/resolvers.js
@@ -1,4 +1,5 @@
 const bcrypt = require('bcryptjs');
+const validator = require('validator');
 
 const User = require('../models/user');
 const UserSChema = require('./schema');
@@ -6,12 +7,24 @@ const UserSChema = require('./schema');
 module.exports = {
     createUser: async function ({ userInput }, req) {
         try {
+            let validationErrors = [];
+
+            if (!validator.isEmail(userInput.email)) {
+                validationErrors.push({ message: "Email address is invalid" });
+            }
+            if (validator.isEmpty(userInput.password) || !validator.isLength(userInput.password, { min: 5 })) {
+                validationErrors.push({ message: "Password is too short" });
+            }
+            if(validationErrors.length > 0){
+                const error = new Error('Invalid input!');
+                throw error;
+            }
             const existingUser = await User.findOne({ email: userInput.email });
             if (existingUser) {
                 const error = new Error("A user with this email exists!");
                 throw error;
             }
-            
+
             const hp = await bcrypt.hashSync(userInput.password, 12);
             const newUser = new User({
                 name: userInput.name,
@@ -19,7 +32,7 @@ module.exports = {
                 password: hp
             });
             const createdUser = await newUser.save();
-            return {...createdUser._doc, _id: createdUser._id.toString() }
+            return { ...createdUser._doc, _id: createdUser._id.toString() }
         } catch (error) {
             console.log(error)
             throw error;
