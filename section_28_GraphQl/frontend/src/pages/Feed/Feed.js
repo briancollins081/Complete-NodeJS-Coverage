f7880d02b4b915d7ee1260a92fdef52b2cf23487
Brian Andere <briancollins081@gmail.com>
2020-03-01 00:01:54 +0300

    A small fix to the pagination

--- section_28_GraphQl/frontend/src/pages/Feed/Feed.js
+++ section_28_GraphQl/frontend/src/pages/Feed/Feed.js
@@ -278,12 +278,15 @@ class Feed extends Component {
         };
         this.setState(prevState => {
           let updatedPosts = [...prevState.posts];
+          let updatedTotalPosts = prevState.totalPosts;
+
           if (prevState.editPost) {
             const postIndex = prevState.posts.findIndex(
               p => p._id === prevState.editPost._id
             );
             updatedPosts[postIndex] = post;
           } else {
+            updatedTotalPosts++;
             if (prevState.posts.length >= 3) {
               updatedPosts.pop();
             }
@@ -293,7 +296,8 @@ class Feed extends Component {
             posts: updatedPosts,
             isEditing: false,
             editPost: null,
-            editLoading: false
+            editLoading: false,
+            totalPosts: updatedTotalPosts
           };
         });
       })
