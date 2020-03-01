4d2543923e50568bceff7fba3c882c22bc82adda
Brian Andere <briancollins081@gmail.com>
2020-03-01 14:38:52 +0300

    Adding ssl key to node server app

--- section_29_deployment/shopapp/app.js
+++ section_29_deployment/shopapp/app.js
@@ -1,4 +1,6 @@
 const path = require('path');
+const fs = require('fs');
+const https = require('https');
 
 const express = require('express');
 const bodyParser = require('body-parser');
@@ -10,6 +12,7 @@ const flash = require('connect-flash');
 const multer = require('multer');
 const helmet = require('helmet');
 const compression = require('compression');
+const morgan = require('morgan');
 
 const errorController = require('./controllers/error');
 const User = require('./models/user');
@@ -39,23 +42,32 @@ const appFileFilter = (req, file, cb) => {
         cb(null, false);
     }
 }
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+const authRoutes = require('./routes/auth');
+const accessLogStream = fs.createWriteStream(
+    path.join(__dirname, 'logs', 'access.log'),
+    { flags: 'a' }
+)
+const privateKey = fs.readFileSync('server.key');
+const certificate = fs.readFileSync('server.cert');
 
 app.set('view engine', 'ejs');
 app.set('views', 'views');
 
-const adminRoutes = require('./routes/admin');
-const shopRoutes = require('./routes/shop');
-const authRoutes = require('./routes/auth');
-
+// http headers security
 app.use(helmet());
-
+// assets compression
 app.use(compression());
+// loggin
+app.use(morgan('combined', { stream: accessLogStream }));
+// ssl encryption
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(multer({ storage: fileStorage, fileFilter: appFileFilter }).single('image')); //'image' is the name of the multipart form field
 
 app.use(express.static(path.join(__dirname, 'public')));
-app.use('/images',express.static(path.join(__dirname, 'images'))); 
+app.use('/images', express.static(path.join(__dirname, 'images')));
 
 app.use(
     session({
@@ -105,7 +117,8 @@ app.use((error, req, res, next) => {
 mongoose
     .connect(MONGODB_URI, { useUnifiedTopology: true, useNewUrlParser: true })
     .then(result => {
-        app.listen(process.env.PORT || 3000);
+        app.listen(process.env.PORT || 3000); //http - default
+        // https.createServer({key: privateKey, cert: certificate}).listen(process.env.PORT || 3000)
     })
     .catch(err => {
         console.log(err);
