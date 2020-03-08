a35a7e2518e96d055fdfb6b7ae03ccfb8eb07104
Brian Andere <briancollins081@gmail.com>
2020-03-08 15:34:29 +0300

    Using hooks for testing

--- section_30_testing_app/backend/test/auth-controller.js
+++ section_30_testing_app/backend/test/auth-controller.js
@@ -5,11 +5,31 @@ const mongoose = require('mongoose');
 const User = require('../models/user');
 const AuthController = require('../controllers/auth');
 
-describe('Auth Controller - Login', function (done) { //done used to test async code
+describe('Auth Controller', function (done) { //done used to test async code
+    //hook
+    before(function (done) {
+        mongoose.connect('mongodb://localhost:27017/test-postmessages', { useUnifiedTopology: true, useNewUrlParser: true })
+            .then(result => {
+                const user = new User({
+                    email: "tester@.test.com",
+                    password: "tester",
+                    name: "Test",
+                    posts: [],
+                    _id: "5e5a5ce79c90176821df17f5"
+                });
+                return user.save();
+            })
+            .then(() => {
+                done();
+            });
+    });
+    beforeEach(function(){
+        console.log("Inside beforeEach()");
+        
+    }); // hook run before each test case i.e. before every it
     it('login middleware should throw an error with code 500 if database access fails', () => {
         sinon.stub(User, 'findOne'); //create a stub for find one
         User.findOne.throws(); //force the stub to throw an error
-
         const req = {
             body: {
                 email: "briancollins081@gmail.com",
@@ -31,46 +51,37 @@ describe('Auth Controller - Login', function (done) { //done used to test async 
     });
 
     it("should send a response with a valid status for an existing user", function (done) {
-        mongoose.connect('mongodb://localhost:27017/test-postmessages', { useUnifiedTopology: true, useNewUrlParser: true })
-            .then(result => {
-                const user = new User({
-                    email: "tester@.test.com",
-                    password: "tester",
-                    name: "Test",
-                    posts: [],
-                    _id: "5e5a5ce79c90176821df17f5"
-                });
-                return user.save();
+        const req = { userId: "5e5a5ce79c90176821df17f5" };
+        const res = {
+            statusCode: 500,
+            userStatus: null,
+            status: function (code) {
+                this.statusCode = code;
+                return this
+            },
+            json: function (data) {
+                this.userStatus = data.status;
+            }
+        };
+        AuthController.getStatus(req, res, () => { }).then(() => {
+            expect(res.statusCode).to.be.equal(200);
+            expect(res.userStatus).to.be.equal('I am new!');
+            done();
+        })
+    });
+
+    after(function (done) {
+        //clean up
+        User.deleteMany({})
+            .then(() => {
+                //diconnect all open handles
+                return mongoose.disconnect();
             })
             .then(() => {
-                const req = { userId: "5e5a5ce79c90176821df17f5" };
-                const res = {
-                    statusCode: 500,
-                    userStatus: null,
-                    status: function (code) {
-                        this.statusCode = code;
-                        return this
-                    },
-                    json: function (data) {
-                        this.userStatus = data.status;
-                    }
-                };
-                AuthController.getStatus(req, res, () => { }).then(() => {
-                    expect(res.statusCode).to.be.equal(200);
-                    expect(res.userStatus).to.be.equal('I am new!');
-                    //clean up
-                    User.deleteMany({})
-                        .then(() => {
-                            //diconnect all open handles
-                            return mongoose.disconnect();
-                        })
-                        .then(() => {
-                            done();
-                        })
-                })
+                done();
             })
-            .catch(err => {
-                console.log(err);
-            });
     });
+    afterEach(function(){
+        console.log("Inside afterEach()");
+    }); //runs after each test block i.e. after every it
 })
